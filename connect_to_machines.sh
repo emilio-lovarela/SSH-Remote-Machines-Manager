f2da8c9fbc528cd4d30d287ed29bacfb94cae572
Emilio López Varela <46821750+emilio-lovarela@users.noreply.github.com>
2024-11-14 18:33:39 +0100

    Update connect_to_machines.sh

--- connect_to_machines.sh
+++ connect_to_machines.sh
@@ -7,6 +7,34 @@ COLOR_RED="\033[1;31m"    # Bright red for dashes
 COLOR_CYAN="\033[1;36m"   # Bright cyan
 COLOR_RESET="\033[0m"     # Reset color
 
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo -e "${COLOR_RED}ERROR${COLOR_RESET}: ${COLOR_GREEN}jq${COLOR_RESET} is not installed."
+    echo -e "Please install it by running: ${COLOR_CYAN}sudo apt-get install jq${COLOR_RESET}"
+    jq_installed=false
+else
+    jq_installed=true
+fi
+
+# Check if pyyaml is installed
+if ! python3 -c "import yaml" &> /dev/null; then
+    echo -e "${COLOR_RED}ERROR${COLOR_RESET}: ${COLOR_GREEN}pyyaml${COLOR_RESET} is not installed."
+    echo -e "Please install it by running: ${COLOR_CYAN}pip install pyyaml${COLOR_RESET}"
+    pyyaml_installed=false
+else
+    pyyaml_installed=true
+fi
+
+# Exit if either dependency is missing
+if [[ "$jq_installed" == false || "$pyyaml_installed" == false ]]; then
+    echo -e "${COLOR_RED}ERROR${COLOR_RESET}: Required dependencies are missing. Please install them and rerun the script."
+    echo -e "Press ${COLOR_RED}Enter${COLOR_RESET} to exit."
+    read
+    exit 1
+fi
+
+
+
 # Path to the configuration file
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_FILE="$DIR/config.yaml"
@@ -55,14 +83,22 @@ for machine in "${machines[@]}"; do
     echo -e "$i) ${COLOR_GREEN}$machine${COLOR_RESET}"
 
     # Extract and display the characteristics of each machine (excluding ip and user)
-    characteristics=$(echo "$config_data" | jq -r ".machines[\"$machine\"] | to_entries | map(select(.key != \"ip\" and .key != \"user\"))")
+    characteristics=$(echo "$config_data" | jq -r ".machines[\"$machine\"] | to_entries | map(select(.key != \"ip\" and .key != \"user\" and .key != \"ProxyJump\"))")
+    ProxyJump=$(echo "$config_data" | jq -r ".machines[\"$machine\"].ProxyJump // empty")
 
+    # Print characteristics with red dashes and the desired indentation
     if [[ -n "$characteristics" ]]; then
-        # Print characteristics with red dashes and the desired indentation
         echo "$characteristics" | jq -r '.[] | "\(.key):\(.value)"' | while IFS=: read -r key value; do
             echo -e "     ${COLOR_RED}- ${COLOR_RESET}$key: ${COLOR_CYAN}$value${COLOR_RESET}"
         done
     fi
+
+    # Print 'ProxyJump' and the intermediate IP if active
+    if [[ -n "$ProxyJump" ]]; then
+        pf_ip=$(echo "$config_data" | jq -r ".machines[\"$machine\"].ProxyJump.ip")
+        echo -e "     ${COLOR_RED}- ${COLOR_RESET}${COLOR_BLUE}ProxyJump:${COLOR_RESET} ${COLOR_CYAN}$pf_ip${COLOR_RESET}"
+    fi
+
     options[$i]=$machine
     ((i++))
 done
@@ -82,7 +118,9 @@ done
 # Get the IP and user of the selected machine
 ip=$(echo "$config_data" | jq -r ".machines[\"$machine\"] | select(.ip and .ip != \"\") | .ip")
 user=$(echo "$config_data" | jq -r ".machines[\"$machine\"] | select(.user and .user != \"\") | .user")
+ProxyJump_active=$(echo "$config_data" | jq -r ".machines[\"$machine\"].ProxyJump // empty")
 
+# Check if user and ip fields in machine selected
 if [[ -z "$ip" || -z "$user" ]]; then
     echo ""
     echo -e "${COLOR_RED}ERROR${COLOR_RESET}"
@@ -93,30 +131,104 @@ if [[ -z "$ip" || -z "$user" ]]; then
     exit 1
 fi
 
-echo ""
-echo -e "Connecting to ${COLOR_GREEN}$machine${COLOR_RESET} (${COLOR_CYAN}$ip${COLOR_RESET}) as (${COLOR_CYAN}$user${COLOR_RESET})"
-echo -e "Maximum timeout ${COLOR_CYAN}$connect_timeout seconds${COLOR_RESET}"
-echo "-----------------------"
+# Check if ProxyJump is enabled and get details if it is
+if [[ -n "$ProxyJump_active" ]]; then
+    pf_user=$(echo "$config_data" | jq -r ".machines[\"$machine\"].ProxyJump.user")
+    pf_ip=$(echo "$config_data" | jq -r ".machines[\"$machine\"].ProxyJump.ip")
+
+    # Rsync backup command
+    rsync_command_prefix="sudo rsync -aAXvz --progress --delete -e '\''ssh -o ProxyJump=$pf_user@$pf_ip'\'' $user@$ip:/home ."
+
+    # Check if pf_user and pf_ip fields are populated
+    if [[ -z "$pf_ip" || -z "$pf_user" || "$pf_ip" == "null" || "$pf_user" == "null" ]]; then
+        echo ""
+        echo -e "${COLOR_RED}ERROR${COLOR_RESET}"
+        echo -e "The ${COLOR_CYAN}ProxyJump: ip${COLOR_RESET} or ${COLOR_CYAN}ProxyJump: user${COLOR_RESET} for the selected machine could not be found"
+        echo -e "Check that the machine ${COLOR_GREEN}($machine)${COLOR_RESET} has the required fields ${COLOR_CYAN}ProxyJump: ip${COLOR_RESET} and ${COLOR_CYAN}ProxyJump: user${COLOR_RESET} in the configuration file."
+        echo -e "Press ${COLOR_RED}Enter${COLOR_RESET} to close the program"
+        read
+        exit 1
+    fi
+
+    echo ""
+    echo -e "Connecting to ${COLOR_GREEN}$machine${COLOR_RESET} (${COLOR_CYAN}$ip${COLOR_RESET}) as (${COLOR_CYAN}$user${COLOR_RESET})"
+    echo -e "Using ${COLOR_GREEN}ProxyJump${COLOR_RESET} (${COLOR_CYAN}$pf_ip${COLOR_RESET}) as (${COLOR_CYAN}$pf_user${COLOR_RESET})"
+    echo -e "Maximum timeout ${COLOR_CYAN}$connect_timeout seconds${COLOR_RESET}"
+    echo "-----------------------"
+else
+    # Rsync backup command
+    rsync_command_prefix="sudo rsync -aAXvz --progress --delete $user@$ip:/home ."
+
+    echo ""
+    echo -e "Connecting to ${COLOR_GREEN}$machine${COLOR_RESET} (${COLOR_CYAN}$ip${COLOR_RESET}) as (${COLOR_CYAN}$user${COLOR_RESET})"
+    echo -e "Maximum timeout ${COLOR_CYAN}$connect_timeout seconds${COLOR_RESET}"
+    echo "-----------------------"
+fi
+
+
 
 
 # SSH key management functions
 check_ssh_key() {
     if [[ -f "$HOME/.ssh/id_rsa" && -f "$HOME/.ssh/id_rsa.pub" ]]; then
-        return 0  # Keys exist
+        # Check if keys exist in the default Windows SSH directory
+        if [ "$FROM_WINDOWS" = "true" ]; then
+            WIN_SSH_PATH="/mnt/c/Users/$WIN_USER/.ssh"
+            if [[ -f "$WIN_SSH_PATH/id_rsa" && -f "$WIN_SSH_PATH/id_rsa.pub" ]]; then
+                return 0 # Keys exist in Windows
+            else
+                copy_ssh_keys_to_windows
+                return 0 # Keys exist
+            fi
+        else
+            return 0  # Keys exist
+        fi
     else
-        return 1  # Keys do not exist
+        # Check if keys exist in the default Windows SSH directory
+        if [ "$FROM_WINDOWS" = "true" ]; then
+            WIN_SSH_PATH="/mnt/c/Users/$WIN_USER/.ssh"
+            if [[ -f "$WIN_SSH_PATH/id_rsa" && -f "$WIN_SSH_PATH/id_rsa.pub" ]]; then
+                echo -e "${COLOR_CYAN}SSH keys${COLOR_RESET} found in Windows directory - ${COLOR_CYAN}$WIN_SSH_PATH${COLOR_RESET}"
+                echo -e "Copying to WSL..."
+                
+                # Copy the keys from Windows to WSL
+                cp "$WIN_SSH_PATH/id_rsa" "$HOME/.ssh/id_rsa"
+                cp "$WIN_SSH_PATH/id_rsa.pub" "$HOME/.ssh/id_rsa.pub"
+                
+                # Change ownership to the WSL user
+                chown "$USER":"$USER" "$HOME/.ssh/id_rsa" "$HOME/.ssh/id_rsa.pub"
+                
+                # Set the appropriate permissions
+                chmod 600 "$HOME/.ssh/id_rsa"
+                chmod 644 "$HOME/.ssh/id_rsa.pub"
+                
+                echo -e "${COLOR_CYAN}SSH keys${COLOR_CYAN} ${COLOR_GREEN}successfully${COLOR_RESET} copied in ${COLOR_GREEN}WSL${COLOR_RESET}"
+                return 0  # Keys exist now in WSL
+            else
+                return 1  # Keys do not exist
+            fi
+        else
+            return 1  # Keys do not exist
+        fi
     fi
 }
 
 generate_ssh_key() {
     echo ""
-    echo -e "No ${COLOR_CYAN}SSH key${COLOR_RESET} was found at $HOME/.ssh/id_rsa"
+    echo -e "No ${COLOR_CYAN}SSH key${COLOR_RESET} was found at ${COLOR_CYAN}$HOME/.ssh/id_rsa${COLOR_RESET}"
+    if [ "$FROM_WINDOWS" = "true" ]; then
+        echo -e "No ${COLOR_CYAN}SSH key${COLOR_RESET} was found at ${COLOR_CYAN}/mnt/c/Users/$WIN_USER/.ssh${COLOR_RESET}"
+    fi
     read -p "Do you want to create an SSH key now? (y/n): " answer
     if [[ "$answer" == "y" || "$answer" == "Y" ]]; then
         ssh-keygen -t rsa -b 4096 -f "$HOME/.ssh/id_rsa" -N ""
         if [[ $? -eq 0 ]]; then
             echo ""
             echo -e "${COLOR_CYAN}SSH key${COLOR_RESET} created ${COLOR_GREEN}successfully${COLOR_RESET}"
+            # Check if the script is run from Windows
+            if [ "$FROM_WINDOWS" = "true" ]; then
+                copy_ssh_keys_to_windows
+            fi
         else
             echo -e "${COLOR_RED}ERROR${COLOR_RESET} creating the ${COLOR_CYAN}SSH key${COLOR_RESET}"
             echo -e "Press ${COLOR_RED}Enter${COLOR_RESET} to close the program"
@@ -136,7 +248,12 @@ generate_ssh_key() {
 copy_ssh_key_to_remote() {
     echo -e "Copying the ${COLOR_CYAN}public key${COLOR_RESET} to the remote server..."
     echo -e "Enter your ${COLOR_GREEN}password${COLOR_RESET} below..."
-    ssh-copy-id -o ConnectTimeout=$connect_timeout $user@$ip
+    if [[ -n "$ProxyJump_active" ]]; then
+        ssh-copy-id -o ProxyJump=$pf_user@$pf_ip -o ConnectTimeout=$connect_timeout $user@$ip
+    else
+        ssh-copy-id -o ConnectTimeout=$connect_timeout $user@$ip
+    fi
+
     if [[ $? -eq 0 ]]; then
         echo -e "${COLOR_CYAN}Public key${COLOR_RESET} copied ${COLOR_GREEN}successfully${COLOR_RESET} to the remote server"
     else
@@ -149,6 +266,23 @@ copy_ssh_key_to_remote() {
     fi
 }
 
+copy_ssh_keys_to_windows() {
+    # Windows SSH directory
+    local win_ssh_path="/mnt/c/Users/$WIN_USER/.ssh"
+    
+    # Create Windows .ssh directory if it doesn't exist
+    if [ ! -d "$win_ssh_path" ]; then
+        echo -e "${COLOR_CYAN}Creating Windows SSH directory...${COLOR_RESET}"
+        mkdir -p "$win_ssh_path"
+    fi
+    
+    # Copy keys to Windows, avoiding overwrites
+    cp --update=none $HOME/.ssh/* "$win_ssh_path/"
+    chmod 600 "$win_ssh_path/id_rsa"
+    chmod 644 "$win_ssh_path/id_rsa.pub"
+    echo -e "${COLOR_CYAN}SSH key${COLOR_RESET} copied to Windows .ssh directory ${COLOR_CYAN}($win_ssh_path)${COLOR_RESET} ${COLOR_GREEN}successfully${COLOR_RESET}"
+}
+
 
 # Prepare the SSH command
 if [[ "$use_ssh_key" == "True" || "$use_ssh_key" == "true" ]]; then
@@ -158,7 +292,32 @@ if [[ "$use_ssh_key" == "True" || "$use_ssh_key" == "true" ]]; then
     fi
 
     # Attempt to connect without password to check if the key is already authorized
-    ssh -o BatchMode=yes -o ConnectTimeout=$connect_timeout $user@$ip 'exit' 2>/dev/null
+    if [[ -n "$ProxyJump_active" ]]; then
+        # Step 1: Check and copy key to the proxy server if ProxyJump is active
+        ssh -o BatchMode=yes -o ConnectTimeout=$connect_timeout $pf_user@$pf_ip 'exit' 2>/dev/null
+        if [[ $? -ne 0 ]]; then
+            # Copy key to proxy server
+            echo ""
+            echo -e "The ${COLOR_CYAN}public key${COLOR_RESET} is not authorized on the proxy server"
+            echo -e "Copying the ${COLOR_CYAN}public key${COLOR_RESET} to the proxy server..."
+            echo -e "Enter your ${COLOR_GREEN}password${COLOR_RESET} below..."
+            ssh-copy-id -o ConnectTimeout=$connect_timeout $pf_user@$pf_ip
+            if [[ $? -eq 0 ]]; then
+                echo -e "${COLOR_CYAN}Public key${COLOR_RESET} copied ${COLOR_GREEN}successfully${COLOR_RESET} to the ProxyJump server (${COLOR_GREEN}$pf_ip${COLOR_RESET})"
+            else
+                echo -e "${COLOR_RED}ERROR${COLOR_RESET} copying the ${COLOR_CYAN}public key${COLOR_RESET} to the ProxyJump server (${COLOR_GREEN}$pf_ip${COLOR_RESET})"
+                echo -e "Press ${COLOR_RED}Enter${COLOR_RESET} to close the program"
+                read
+                exit 1
+            fi
+        fi
+        # Step 2: Check if key is in the final server
+        ssh -o ProxyJump=$pf_user@$pf_ip -o BatchMode=yes -o ConnectTimeout=$connect_timeout $user@$ip 'exit' 2>/dev/null
+    else
+        ssh -o BatchMode=yes -o ConnectTimeout=$connect_timeout $user@$ip 'exit' 2>/dev/null
+    fi
+
+    # Copy key to remote server
     if [[ $? -ne 0 ]]; then
         echo ""
         echo -e "The ${COLOR_CYAN}public key${COLOR_RESET} is not authorized on the remote server"
@@ -166,12 +325,22 @@ if [[ "$use_ssh_key" == "True" || "$use_ssh_key" == "true" ]]; then
     fi
 
     # Now connect using the SSH key
-    ssh_command="ssh -i $HOME/.ssh/id_rsa -o ConnectTimeout=$connect_timeout -t $user@$ip"
+    ssh_command="ssh -i $HOME/.ssh/id_rsa -o ConnectTimeout=$connect_timeout -t"
 else
     # Normal password-based connection
-    ssh_command="ssh -o ConnectTimeout=$connect_timeout -o PubkeyAuthentication=no -o PreferredAuthentications=password -o IdentitiesOnly=yes -t $user@$ip"
+    ssh_command="ssh -o ConnectTimeout=$connect_timeout -o PubkeyAuthentication=no -o PreferredAuthentications=password -o IdentitiesOnly=yes -t"
 fi
 
+# Post forwarding option if exist
+if [[ -n "$ProxyJump_active" ]]; then
+    # Connect through intermediate machine
+    ssh_command="$ssh_command -J $pf_user@$pf_ip $user@$ip"
+    scp_command_prefix="scp -o ProxyJump=$pf_user@$pf_ip"
+else
+    # Direct SSH connection
+    ssh_command="$ssh_command $user@$ip"
+    scp_command_prefix="scp"
+fi
 
 # Check if export_commands_to_machine is True or False
 if [[ "$export_commands_to_machine" == "true" ]]; then
@@ -186,13 +355,25 @@ if [[ "$export_commands_to_machine" == "true" ]]; then
         # Category: Transfer files between machines
         echo -e \" - \033[1;32mTransfer files between machines\033[0m\";
         echo -e \"   \033[1;31m#\033[0m Send a file to the remote machine (run on local machine)\";
-        echo -e \"    \033[1;36m scp example.zip $user@$ip:example_folder/\033[0m\";
+        echo -e \"    \033[1;36m $scp_command_prefix example.zip $user@$ip:example_folder/\033[0m\";
         echo -e \"   \033[1;31m#\033[0m Retrieve a file from the remote machine (run on local machine)\";
-        echo -e \"    \033[1;36m scp $user@$ip:example/example.zip .\033[0m\";
+        echo -e \"    \033[1;36m $scp_command_prefix $user@$ip:example/example.zip .\033[0m\";
+        echo -e \"   \033[1;32m  ----\033[0m\";
         echo -e \"   \033[1;31m#\033[0m Create zip of a folder\";
         echo -e \"    \033[1;36m zip -r my_project.zip my_project/\033[0m\";
         echo -e \"   \033[1;31m#\033[0m Unzip file\";
         echo -e \"    \033[1;36m unzip file.zip -d destination_folder\033[0m\";
+        echo -e \"   \033[1;32m  ----\033[0m\";
+        echo -e \"   \033[1;31m#\033[0m Displays disk usage for each top-level folder in the current directory\";
+        echo -e \"    \033[1;36m sudo du -h --max-depth=1\033[0m\";
+        echo -e \"      \033[1;31m·\033[0m Shows sizes in human-readable format \033[1;36m-h\033[0m\"
+        echo -e \"      \033[1;31m·\033[0m Limits output to top-level folders   \033[1;36m--max-depth=1\033[0m\";
+        echo -e \"   \033[1;31m#\033[0m Create or synchronize backup from the remote machine (run on local machine)\";
+        echo -e \"    \033[1;36m $rsync_command_prefix\033[0m\";
+        echo -e \"      \033[1;31m·\033[0m Test backup without copying      \033[1;36m--dry-run\033[0m\";
+        echo -e \"      \033[1;31m·\033[0m Exclude hidden and config files  \033[1;36m--exclude='\''.*'\''\033[0m\"
+        echo -e \"      \033[1;31m·\033[0m Exclude paths defined in file    \033[1;36m--exclude-from=<file>\033[0m\"
+        echo -e \"      \033[1;31m·\033[0m Removes files in the destination \033[1;36m--delete\033[0m\";
 
         # Category: Manage processes
         echo ""
@@ -201,6 +382,7 @@ if [[ "$export_commands_to_machine" == "true" ]]; then
         echo -e \"    \033[1;36m nohup python3 -u train.py ^> train.log ^&\033[0m\";
         echo -e \"   \033[1;31m#\033[0m View live console using log file\";
         echo -e \"    \033[1;36m tail -f train.log\033[0m\";
+        echo -e \"   \033[1;32m  ----\033[0m\";
         echo -e \"   \033[1;31m#\033[0m Find general pid\";
         echo -e \"    \033[1;36m ps\033[0m\";
         echo -e \"   \033[1;31m#\033[0m Find specific pid (python processes from the user)\";
@@ -265,9 +447,11 @@ fi
 # Check if the SSH was successful
 if [[ $? -ne 0 ]]; then
     echo ""
-    echo -e "${COLOR_RED}ERROR${COLOR_RESET}: Could not connect to the machine (${COLOR_GREEN}$machine${COLOR_RESET})"
+    echo -e "${COLOR_RED}ERROR${COLOR_RESET}: Unable to connect to the machine (${COLOR_GREEN}$machine${COLOR_RESET})"
     echo -e "Verify that the ip (${COLOR_CYAN}$ip${COLOR_RESET}) and user (${COLOR_CYAN}$user${COLOR_RESET}) are correct"
     echo -e "Verify that the machine (${COLOR_GREEN}$machine${COLOR_RESET}) is accessible with the command - ${COLOR_CYAN}ping $ip${COLOR_RESET}"
+    if [[ -n "$ProxyJump_active" ]]; then
+        echo -e "Check if connecting to the intermediate machine is possible. You may need to connect to a VPN"
     echo -e "Press ${COLOR_RED}Enter${COLOR_RESET} to close the program"
     read  # This will make the script wait for user input before continuing
 fi
